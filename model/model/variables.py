2746a12f949e892b0d6c1f5d5e70d704bc2cd801
FenikL <fedotov.ia15@physics.msu.ru>
2021-04-22 21:40:45 +0300

    updata t_on and t_off

--- model/model/variables.py
+++ model/model/variables.py
@@ -14,8 +14,8 @@ AREA_LENGTH = 12 #8
 NUM_TAGS = 30
 INTERVAL = 1
 DR = 64 / 3
-t_on = 2000e-3
-t_off = 100e-3
+t_on = 100e-3
+t_off = 320e-3
 MICRO = 0.000001
 
 DurationFromReader = namedtuple('DurationFromReader', ['query', 'qrep', 'ack', 'req_rn', 'read'])
